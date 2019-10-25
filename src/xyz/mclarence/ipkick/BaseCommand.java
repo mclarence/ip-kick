d528b8e75545b2f72ce33041d37a5f1169bfd3cf
Clarence Mesina <cmesina@Clarences-MacBook-Pro.local>
2019-10-25 11:05:20 +1100

    Add permissions

--- src/xyz/mclarence/ipkick/BaseCommand.java
+++ src/xyz/mclarence/ipkick/BaseCommand.java
@@ -2,11 +2,19 @@ package xyz.mclarence.ipkick;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class BaseCommand implements CommandInterface {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage("Base command");
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (!p.hasPermission("ipkick.base")) {
+                p.sendMessage("You do not have permission to access this command");
+                return false;
+            }
+        }
+        sender.sendMessage("IP Kick version 1. Reload configuration with /ipkick reload");
         return true;
     }
 }
--- src/xyz/mclarence/ipkick/ReloadCommand.java
+++ src/xyz/mclarence/ipkick/ReloadCommand.java
@@ -2,6 +2,7 @@ package xyz.mclarence.ipkick;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ReloadCommand implements CommandInterface {
 
@@ -12,11 +13,18 @@ public class ReloadCommand implements CommandInterface {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd,
-                             String commandLabel, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (!p.hasPermission("ipkick.reload")) {
+                p.sendMessage("You do not have permission to access this command");
+                return false;
+            }
+        }
 
         plugin.reloadConfiguration();
         sender.sendMessage("Reloaded IP Kick");
-        return false;
+        return true;
     }
 }
